a947d78834c06756ee391ec0865eae98b36efd3e
Elian Hirth <elianhirth.t@gmail.com>
2025-05-05 16:13:09 +0200

    Ajout fonction translate, resize, rotate, equals

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -46,3 +46,35 @@ void Square::draw() {
     std::vector<Point> points = {A, B, C, D, A};
     draw_picture(points);
 }
+
+void Square::translate(Point T) {
+    A.x += T.x;
+    A.y += T.y;
+    C.x += T.x;
+    C.y += T.y;
+}
+
+void Square::resize(double ratio) {
+    A.x *= ratio;
+    A.y *= ratio;
+    C.x *= ratio;
+    C.y *= ratio;
+}
+
+void Square::rotate(double angle) {
+    double x = A.x;
+    double y = A.y;
+    A.x = x * cos(angle) - y * sin(angle);
+    A.y = x * sin(angle) + y * cos(angle);
+    x = C.x;
+    y = C.y;
+    C.x = x * cos(angle) - y * sin(angle);
+    C.y = x * sin(angle) + y * cos(angle);
+}
+
+bool Square::equals(Square square) {
+    Point B, D, B_square, D_square;
+    find_other_corner(A, C, B, D);
+    find_other_corner(square.A, square.C, B_square, D_square);
+    return (A.x == square.A.x && A.y == square.A.y && C.x == square.C.x && C.y == square.C.y) && (B.x == B_square.x && B.y == B_square.y && D.x == D_square.x && D.y == D_square.y);
+}
