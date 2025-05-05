bfcd1a51e4c03843902061f833dee1d9df3c2265
Mathias <mathiasluck2@gmail.com>
2025-05-05 15:42:33 +0200

    Updated tests

--- main.cpp
+++ main.cpp
@@ -23,9 +23,34 @@ int main() {
 		
 	};
 	
+	////////////////////////////// Tests /////////////////////////////////
+	// Tests distance
 	assert(points.at(0).distance(points.at(1)) == 100);
 	assert(points.at(1).distance(points.at(2)) == 100);
 	assert( equals( points.at(0).distance(points.at(2)), sqrt(100*100 + 100*100) ) );
+	
+	// Tests square 
+	Square square(points.at(0), points.at(3));
+	assert( equals( square.side(), sqrt(50*50+50*50) ) );
+	assert( equals( square.perimeter(), 4*sqrt(50*50+50*50) ) );
+	assert( equals( square.area(), 50*50+50*50) );
+	assert(square.center().x == 0);
+	assert(square.center().y == 50);
+
+	Square square2(points.at(0), points.at(2));
+	assert( equals( square2.side(), sqrt(100) ) );
+	assert( equals( square2.perimeter(), 4*sqrt(100) ) );
+	assert( equals( square2.area(), 100*100) );
+	assert(square2.center().x == 50);
+	assert(square2.center().y == 50);
+
+	
+	// Tests triangle
+	Triangle triangle(points.at(0), points.at(1), points.at(2));
+	assert(triangle.perimeter() == 300);
+	assert(triangle.area() == 10000);
+	assert(triangle.center().x == 50);
+	assert(triangle.center().y == 50);
 
 	// Draw the picture by connecting the points
 	// draw_picture(points);
