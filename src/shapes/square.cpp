18a0caac4cd77129155d159c08fefc07b8d79eeb
Elian Hirth <elianhirth.t@gmail.com>
2025-05-05 15:14:36 +0200

    Ajout de périmètre, aire et centre

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -7,3 +7,16 @@ Square::Square(Point P, Point R) : A(P), C(R) {};
 double Square::side() {
     return (1/sqrt(2))*A.distance(C);
 }
+
+double Square::perimeter() {
+    return 4*side();
+}
+
+double Square::area() {
+    return side()*side();
+}
+
+Point Square::center() {
+    return Point((A.x+C.x)/2, (A.y+C.y)/2);
+}
+
