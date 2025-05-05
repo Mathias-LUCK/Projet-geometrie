0cd68d5c47a4cdee32405004d378a856026ce7e6
Elian Hirth <elianhirth.t@gmail.com>
2025-05-05 15:04:20 +0200

    Avec fonction side square

--- src/shapes/square.cpp
+++ src/shapes/square.cpp
@@ -1,4 +1,9 @@
 #include "point.hpp"
 #include "shapes/square.hpp"
+#include <math.h>
 
 Square::Square(Point P, Point R) : A(P), C(R) {};
+
+double Square::side() {
+    return (1/sqrt(2))*A.distance(C);
+}
