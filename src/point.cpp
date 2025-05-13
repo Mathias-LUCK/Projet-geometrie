ebff90696dcd6d3f70d5e3090c8d873ec2eba4f5
Mathias <mathiasluck2@gmail.com>
2025-05-13 11:53:19 +0200

    Ajout du readme, et de equals pour les points

--- src/point.cpp
+++ src/point.cpp
@@ -5,3 +5,7 @@ Point::Point(double x_val, double y_val) : x(x_val), y(y_val) {}
 double Point::distance(Point B) {
     return sqrt((B.x - x) * (B.x - x) + (B.y - y) * (B.y - y));
 };
+
+bool Point::equals(Point B) { 
+    return x == B.x && y == B.y; 
+}
